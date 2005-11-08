a04fb58a608282e7dab80b0644d8100947642867
Candid Dauth <cdauth@cdauth.de>
2005-11-08 15:54:51 +0000

    git-svn-id: file:///srv/svn/sua@59 4c599e6c-1f03-0410-aeda-aa0b023f3d7a

--- login/highscores.php
+++ login/highscores.php
@@ -20,10 +20,16 @@
 	while($bracket = fread($fh, 32))
 	{
 		$info = highscores::get_info($bracket);
+
+		$class = 'fremd';
+		if($info[0] == $_SESSION['username'])
+			$class = 'eigen';
+		elseif(in_array($info[0], $user_array['verbuendete']))
+			$class = 'verbuendet';
 ?>
-		<tr>
+		<tr class="<?=$class?>">
 			<th class="c-platz"><?=ths($platz)?></th>
-			<td class="c-spieler"><a href="help/playerinfo.php?player=<?=htmlentities(urlencode($info[0]))?>" title="Informationen zu diesem Spieler anzeigen"><?=utf8_htmlentities($info[0])?></a> <a href="nachrichten.php?to=<?=htmlentities(urlencode($info[0]))?>" title="Schreiben Sie diesem Spieler eine Nachricht" class="nachricht-schreiben">[N]</a></td>
+			<td class="c-spieler"><a href="help/playerinfo.php?player=<?=htmlentities(urlencode($info[0]))?>" title="Informationen zu diesem Spieler anzeigen" class="playername"><?=utf8_htmlentities($info[0])?></a> <a href="nachrichten.php?to=<?=htmlentities(urlencode($info[0]))?>" title="Schreiben Sie diesem Spieler eine Nachricht" class="nachricht-schreiben">[N]</a></td>
 			<td class="c-punktzahl"><?=ths($info[1])?></td>
 		</tr>
 <?php
