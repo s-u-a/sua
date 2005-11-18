0db56e4e6c5a4aac9bdf360cec94e2849085ba04
René Müller <rmueller@s-u-a.net>
2005-11-18 22:19:19 +0000

    git-svn-id: file:///srv/svn/sua@100 4c599e6c-1f03-0410-aeda-aa0b023f3d7a

--- rules.php
+++ rules.php
@@ -4,6 +4,9 @@
 	gui::html_head();
 ?>
 <h2><abbr title="Stars Under Attack" xml:lang="en">S-U-A</abbr> &ndash; Regeln</h2>
+<h3>§&thinsp;1: Allgemeine Nutzungsbedingungen</h3>
+<ol>
+	<li>
 <?php
 	gui::html_foot();
 ?>
