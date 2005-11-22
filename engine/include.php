ff5e92ce253c25aed3dea0d6407a920369a5348a
Candid Dauth <cdauth@cdauth.de>
2005-11-22 12:50:00 +0000

    git-svn-id: file:///srv/svn/sua@143 4c599e6c-1f03-0410-aeda-aa0b023f3d7a

--- engine/include.php
+++ engine/include.php
@@ -574,7 +574,7 @@
 
 		function get_planet_class($galaxy, $system, $planet)
 		{
-			$type = (((floor($system/100)+1)*(floor(($system%100)/10)+1)*(($system%10)+1)+$galaxy)%$planet)*$planet+$system;
+			$type = (((floor($system/100)+1)*(floor(($system%100)/10)+1)*(($system%10)+1))%$planet)*$planet+($system%+$galaxy);
 			return $type%20+1;
 		}
 	}
