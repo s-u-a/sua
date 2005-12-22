f35c80ed7dfc6abc1207c1598cfee2f6ec46e7ff
Candid Dauth <cdauth@cdauth.de>
2005-12-22 12:41:18 +0000

    git-svn-id: file:///srv/svn/sua@303 4c599e6c-1f03-0410-aeda-aa0b023f3d7a

--- passwd.php
+++ passwd.php
@@ -1,8 +1,12 @@
 <?php
 	require('engine/include.php');
+	
+	$databases = get_databases();
 
-	if(isset($_POST['benutzername']) && isset($_POST['email']))
+	if(isset($_POST['benutzername']) && isset($_POST['email']) && isset($_POST['database']) && isset($databases[$_POST['database']]))
 	{
+		define_globals($databases[$_POST['database']][0]);
+		
 		$_POST['benutzername'] = trim($_POST['benutzername']);
 		$_POST['email'] = trim($_POST['email']);
 		if(!is_file(DB_PLAYERS.'/'.urlencode($_POST['benutzername'])) || !is_readable(DB_PLAYERS.'/'.urlencode($_POST['benutzername'])))
@@ -21,7 +25,7 @@
 				$that_user_array['email_passwd'] = $send_id;
 				if(!write_user_array($_POST['benutzername'], $that_user_array))
 					$error = 'Datenbankfehler 2.';
-				elseif(!mail($that_user_array['email'], 'Passwort�nderung in S-U-A', "Jemand (vermutlich Sie) hat in S-U-A die \xe2\x80\x9ePasswort vergessen\xe2\x80\x9c-Funktion mit Ihrem Account benutzt. Diese Nachricht ist deshalb an jene E-Mail-Adresse adressiert, die Sie in Ihren Einstellungen in S-U-A eingetragen haben.\nSollten Sie eine Änderung Ihres Passworts nicht erwünschen, ignorieren – oder besser löschen – Sie diese Nachricht einfach.\n\nUm Ihr Passwort zu ändern, rufen Sie bitte die folgende Adresse in Ihrem Browser auf und folgen Sie den Anweisungen:\nhttps://".$_SERVER['HTTP_HOST'].h_root."/passwd.php?name=".urlencode($_POST['benutzername'])."&id=".urlencode($send_id)."\n(Ohne SSL: http://".$_SERVER['HTTP_HOST'].h_root."/passwd.php?name=".urlencode($_POST['benutzername'])."&id=".urlencode($send_id), "Content-Type: text/plain;\r\n  charset=\"utf-8\"\r\nFrom: ".$EMAIL_FROM."\r\nReply-To: ".$EMAIL_FROM))
+				elseif(!mail($that_user_array['email'], 'Passwort�nderung in S-U-A', "Jemand (vermutlich Sie) hat in S-U-A die \xe2\x80\x9ePasswort vergessen\xe2\x80\x9c-Funktion mit Ihrem Account benutzt. Diese Nachricht ist deshalb an jene E-Mail-Adresse adressiert, die Sie in Ihren Einstellungen in S-U-A eingetragen haben.\nSollten Sie eine Änderung Ihres Passworts nicht erwünschen, ignorieren – oder besser löschen – Sie diese Nachricht einfach.\n\nUm Ihr Passwort zu ändern, rufen Sie bitte die folgende Adresse in Ihrem Browser auf und folgen Sie den Anweisungen:\nhttps://".$_SERVER['HTTP_HOST'].h_root."/passwd.php?name=".urlencode($_POST['benutzername'])."&id=".urlencode($send_id)."\n(Ohne SSL: http://".$_SERVER['HTTP_HOST'].h_root."/passwd.php?name=".urlencode($_POST['benutzername'])."&id=".urlencode($send_id)."&database=".urlencode($_POST['database']), "Content-Type: text/plain;\r\n  charset=\"utf-8\"\r\nFrom: ".$EMAIL_FROM."\r\nReply-To: ".$EMAIL_FROM))
 					$error = 'Fehler beim Versand der E-Mail-Nachricht.';
 				else
 				{
@@ -43,8 +47,10 @@
 ?>
 <h2><abbr title="Stars Under Attack" xml:lang="en">S-U-A</abbr> &ndash; Passwort vergessen</h2>
 <?php
-	if(isset($_GET['name']) && isset($_GET['id']))
+	if(isset($_GET['name']) && isset($_GET['id']) && isset($_GET['database']) && isset($databases[$_GET['database']]))
 	{
+		define_globals($databases[$_GET['database']][0]);
+		
 		if(!is_file(DB_PLAYERS.'/'.urlencode($_GET['name'])) || !is_readable(DB_PLAYERS.'/'.urlencode($_GET['name'])))
 		{
 ?>
@@ -140,6 +146,18 @@
 <p>Um Ihr Passwort �ndern zu k�nnen, f�llen Sie bitte in das folgende Formular Ihren Benutzernamen und diejenige <span xml:lang="en">E-Mail</span>-Adresse an, die Sie im Spiel in Ihren Einstellungen gespeichert haben.</p>
 <form action="<?=htmlentities(USE_PROTOCOL.'://'.$_SERVER['HTTP_HOST'].h_root.'/passwd.php')?>" method="post">
 	<dl>
+		<dt><label for="runde-select">Runde</label></dt>
+		<dd><select name="database" id="runde-select">
+<?php
+		foreach($databases as $id=>$info)
+		{
+?>
+			<option value="<?=htmlentities($id)?>"><?=utf8_htmlentities($info[1])?></option>
+<?php
+		}
+?>
+		</select></dd>
+		
 		<dt><label for="benutzername-input">Benutzername</label></dt>
 		<dd><input type="text" name="benutzername" id="benutzername-input" /></dd>
 
