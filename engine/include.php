fa27157258b5d88c108305b0a2d64fab315fca54
Candid Dauth <cdauth@cdauth.de>
2006-04-29 00:24:34 +0000

    Document-Root-Fix.
    git-svn-id: file:///srv/svn/sua@592 4c599e6c-1f03-0410-aeda-aa0b023f3d7a

--- engine/include.php
+++ engine/include.php
@@ -13,7 +13,12 @@
 		exit(1);
 	}
 	define('s_root', substr($__FILE__, 0, -strlen($this_filename)));
-	$document_root = $_SERVER['DOCUMENT_ROOT'];
+	if(isset($_SERVER['SCRIPT_FILENAME']) && isset($_SERVER['PHP_SELF']) && substr($_SERVER['SCRIPT_FILENAME'], -strlen($_SERVER['PHP_SELF'])) == $_SERVER['PHP_SELF'])
+		$document_root = substr($_SERVER['SCRIPT_FILENAME'], 0, -strlen($_SERVER['PHP_SELF']));
+	elseif(isset($_SERVER['DOCUMENT_ROOT']))
+		$document_root = $_SERVER['DOCUMENT_ROOT'];
+	else $document_root = '/';
+
 	if(substr($document_root, -1) == '/')
 		$document_root = substr($document_root, 0, -1);
 	define('h_root', substr(s_root, strlen($document_root)));
