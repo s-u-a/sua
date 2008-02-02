19c77675159f7381c870b8f903a421d4f3e217d7
Candid Dauth <cdauth@cdauth.de>
2008-02-02 20:15:12 +0000

    Fast planet switching did not work.
    git-svn-id: file:///srv/svn/sua@1046 4c599e6c-1f03-0410-aeda-aa0b023f3d7a

--- login/scripts/include.php
+++ login/scripts/include.php
@@ -381,7 +381,7 @@
 			if($active_planet_1 > max($me->getPlanetsList())) $active_planet_1 = 0;
 ?>
 				<hr class="separator" />
-				<h1><a href="?<?=htmlspecialchars("planet=".urlencode($active_planet_0)."&".global_setting("URL_SUFFIX"))?>" title="<?=h(_("Zum vorigen Planeten wechseln&[login/scripts/include.php|1]"), false)?>"<?=accesskey_attr(_("Zum vorigen Planeten wechseln&[login/scripts/include.php|1]"))?>><?=h(_("←"))?></a> <?=sprintf(h(_("„%s“ (%s)")), htmlspecialchars($me->planetName()), vsprintf(h(_("%d:%d:%d")), $me->getPos()))?> <a href="?<?=htmlspecialchars("planet=".urlencode($active_planet_1)."&".global_setting("URL_SUFFIX"))?>" title="<?=h(_("Zum nächsten Planeten wechseln&[login/scripts/include.php|1]"), false)?>"<?=accesskey_attr(_("Zum nächsten Planeten wechseln&[login/scripts/include.php|1]"))?>><?=h(_("→"))?></a></h1>
+				<h1><a href="?<?=htmlspecialchars(preg_replace("/((^|&)planet=)\d+/", "\${1}".$active_planet_0, global_setting("URL_SUFFIX")))?>" title="<?=h(_("Zum vorigen Planeten wechseln&[login/scripts/include.php|1]"), false)?>"<?=accesskey_attr(_("Zum vorigen Planeten wechseln&[login/scripts/include.php|1]"))?>><?=h(_("←"))?></a> <?=sprintf(h(_("„%s“ (%s)")), htmlspecialchars($me->planetName()), vsprintf(h(_("%d:%d:%d")), $me->getPos()))?> <a href="?<?=htmlspecialchars(preg_replace("/((^|&)planet=)\d+/", "\${1}".$active_planet_1, global_setting("URL_SUFFIX")))?>" title="<?=h(_("Zum nächsten Planeten wechseln&[login/scripts/include.php|1]"), false)?>"<?=accesskey_attr(_("Zum nächsten Planeten wechseln&[login/scripts/include.php|1]"))?>><?=h(_("→"))?></a></h1>
 <?php
 			if(isset($options["notify"]) && $options["notify"] || !isset($options["notify"]) && $me->checkSetting('notify'))
 			{
