a7ec255859e0fc2d2afb11c0a206288933959266
Candid Dauth <cdauth@cdauth.de>
2009-11-21 19:53:42 +0100

    Fixed roboter.php.

--- game/roboter.php
+++ game/roboter.php
@@ -23,44 +23,47 @@
 	*/
 	namespace sua\psua;
 
+	use \sua\L;
+	use \sua\F;
+
 	require('include.php');
 
 	if(isset($_POST['cancel-all-roboter']))
 	{
-		if($me->checkPassword($_POST['cancel-all-roboter']) && $me->removeBuildingThing('roboter', true))
+		if($USER->checkPassword($_POST['cancel-all-roboter']) && $USER->removeBuildingThing('roboter', true))
 			delete_request();
 	}
 
-	if($me->permissionToAct() && isset($_POST['roboter']) && is_array($_POST['roboter']))
+	if($USER->permissionToAct() && isset($_POST['roboter']) && is_array($_POST['roboter']))
 	{
 		# Roboter in Auftrag geben
 		$built = 0;
 		foreach($_POST['roboter'] as $id=>$count)
 		{
-			if($me->buildRoboter($id, $count)) $built++;
+			if($PLANET->buildRoboter($id, $count)) $built++;
 		}
 		if($built > 0)
 			delete_request();
 	}
 
-	$gui->init();
+	$GUI->init();
 ?>
 <h2><?=L::h(_("Roboter"))?></h2>
-<form action="roboter.php?<?=htmlspecialchars(global_setting("URL_SUFFIX"))?>" method="post">
+<form action="roboter.php?<?=htmlspecialchars($GUI->getOption("url_suffix"))?>" method="post">
 <?php
-	$roboter = $me->getItemsList('roboter');
-	$building_possible = (!($building_gebaeude = $me->checkBuildingThing('gebaeude')) || $building_gebaeude[0] != 'B9');
+	$roboter = $USER->getItemsList('roboter');
+	$building_possible = (!($building_gebaeude = $PLANET->checkBuildingThing('gebaeude')) || $building_gebaeude[0] != 'B9');
 	foreach($roboter as $id)
 	{
-		$item_info = $me->getItemInfo($id, "roboter", array("buildable", "level", "name", "ress", "time"));
+		$item_info = $USER->getItemInfo($id, "roboter", array("buildable", "level", "name", "ress", "time"), $PLANET);
 
 		if(!$item_info['buildable'] && $item_info['level'] <= 0)
 			continue;
 ?>
 	<div class="item roboter" id="item-<?=htmlspecialchars($id)?>">
-		<h3><a href="info/description.php?id=<?=htmlspecialchars(urlencode($id))?>&amp;<?=htmlspecialchars(global_setting("URL_SUFFIX"))?>" title="<?=L::h(_("Genauere Informationen anzeigen"))?>"><?=htmlspecialchars($item_info['name'])?></a> <span class="anzahl">(<?=htmlspecialchars($item_info['level'])?>)</span></h3>
+		<h3><a href="info/description.php?id=<?=htmlspecialchars(urlencode($id))?>&amp;<?=htmlspecialchars($GUI->getOption("url_suffix"))?>" title="<?=L::h(_("Genauere Informationen anzeigen"))?>"><?=htmlspecialchars($item_info['name'])?></a> <span class="anzahl">(<?=htmlspecialchars($item_info['level'])?>)</span></h3>
 <?php
-		if($me->permissionToAct() && $building_possible && $item_info['buildable'])
+		if($USER->permissionToAct() && $building_possible && $item_info['buildable'])
 		{
 ?>
 		<ul>
@@ -73,7 +76,7 @@
 			<dt class="item-kosten"><?=L::h(_("Kosten"))?></dt>
 			<dd class="item-kosten">
 <?php
-		echo F::formatRess($item_info['ress'], 4, false, false, false, $me);
+		echo F::formatRess($item_info['ress'], 4, false, false, false, $PLANET);
 ?>
 			</dd>
 
@@ -93,7 +96,7 @@
 ?>
 </form>
 <?php
-	$building_roboter = $me->checkBuildingThing('roboter');
+	$building_roboter = $PLANET->checkBuildingThing('roboter');
 	if(count($building_roboter) > 0)
 	{
 ?>
@@ -109,7 +112,7 @@
 		$first_building[2]--;
 		if($first_building[2] <= 0) array_shift($building_roboter);
 ?>
-	<li class="<?=htmlspecialchars($first[0])?> active<?=(count($building_roboter) <= 0) ? ' last' : ''?>"><strong><?=L::h(_("[item_".$first[0]."]"))?> <span class="restbauzeit" id="restbauzeit-<?=$i++?>"><?=htmlspecialchars(F::formatFTime($first[1], $me))?></span></strong></li>
+	<li class="<?=htmlspecialchars($first[0])?> active<?=(count($building_roboter) <= 0) ? ' last' : ''?>"><strong><?=L::h(_("[item_".$first[0]."]"))?> <span class="restbauzeit" id="restbauzeit-<?=$i++?>"><?=htmlspecialchars(F::formatFTime($first[1], $USER))?></span></strong></li>
 <?php
 		if(count($building_roboter) > 0)
 		{
@@ -119,14 +122,14 @@
 			{
 				$finishing_time = $bau[1]+$bau[2]*$bau[3];
 ?>
-	<li class="<?=htmlspecialchars($bau[0])?><?=($key == $last) ? ' last' : ''?>"><?=L::h(_("[item_".$bau[0]."]"))?> &times; <?=$bau[2]?><?php if($key == $last){?> <span class="restbauzeit" id="restbauzeit-<?=$i++?>"><?=htmlspecialchars(F::formatFTime($finishing_time, $me))?></span><?php }?></li>
+	<li class="<?=htmlspecialchars($bau[0])?><?=($key == $last) ? ' last' : ''?>"><?=L::h(_("[item_".$bau[0]."]"))?> &times; <?=$bau[2]?><?php if($key == $last){?> <span class="restbauzeit" id="restbauzeit-<?=$i++?>"><?=htmlspecialchars(F::formatFTime($finishing_time, $USER))?></span><?php }?></li>
 <?php
 			}
 		}
 ?>
 </ol>
 <?php
-		if(!$me->umode())
+		if(!$USER->umode())
 		{
 ?>
 <script type="text/javascript">
@@ -143,12 +146,11 @@
 <?php
 		}
 ?>
-<form action="<?=htmlspecialchars(global_setting("USE_PROTOCOL").'://'.$_SERVER['HTTP_HOST'].global_setting("h_root").'/login/roboter.php?'.global_setting("URL_SUFFIX"))?>" method="post" class="alle-abbrechen">
+<form action="<?=htmlspecialchars(global_setting("USE_PROTOCOL").'://'.$_SERVER['HTTP_HOST'].global_setting("h_root").'/login/roboter.php?'.$GUI->getOption("url_suffix"))?>" method="post" class="alle-abbrechen">
 	<p><?=sprintf(h(_("Geben Sie hier Ihr Passwort ein, um alle im Bau befindlichen Roboter %sohne Kostenrückerstattung%s abzubrechen.")), "<strong>", "</strong>")?></p>
 	<div><input type="password" name="cancel-all-roboter" /><input type="submit" value="<?=L::h(_("Alle abbrechen&[login/roboter.php|1]"), false)?>"<?=L::accesskeyAttr(_("Alle abbrechen&[login/roboter.php|1]"))?> /></div>
 </form>
 <?php
 	}
 
-	$gui->end();
-?>
+	$GUI->end();
